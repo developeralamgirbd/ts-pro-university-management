dfdc5a1312bf7bf69c59b98d9a8430876e735269
Alamgir Hossen <96404769+developeralamgirbd@users.noreply.github.com>
2023-06-15 05:37:34 +0600

    [Done] basic project setup

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
 
 
-const main = async()=>{
+const port = config.port as string || 8000
+const boostrap = async()=>{
     try{
-        await mongoose.connect("mongodb+srv://alamgir:Az6908819789@cluster0.9inlzqr.mongodb.net/university-management")
+        await mongoose.connect(config.database_url as string)
         console.log(`DB is connected successfully`);
-        
+        app.listen(port, () => {
+            console.log(`app listening on port ${port}`)
+          })
     }catch(e){
         console.log(e);
         
     }
 }
 
-main()
+boostrap()
