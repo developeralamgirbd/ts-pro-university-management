35a68417a1aa27890c6b5ef697a51aacd65c651b
Alamgir Hossen <96404769+developeralamgirbd@users.noreply.github.com>
2023-06-15 21:11:24 +0600

    setup eslint prettier husky lint-staged

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
+
+const port = (config.port as string) || 8000
+const boostrap = async () => {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log(`DB is connected successfully`)
+    app.listen(port, () => {
+      console.log(`app listening on port ${port}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+boostrap()
