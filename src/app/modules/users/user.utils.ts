be3d79455b2e1d6b710632c4f3503406253fc862
Alamgir Hossen <96404769+developeralamgirbd@users.noreply.github.com>
2023-06-23 16:45:28 +0600

    [Done] user create by generate auto increement id

---
+++ src/app/modules/users/user.utils.ts
@@ -0,0 +1,8 @@
+import { findLastUser } from './users.service'
+
+export const generateUserId = async (): Promise<string> => {
+  const currentId = (await findLastUser()) || (0).toString().padStart(5, '0')
+  // increement id by 1
+  const increementalId = (parseInt(currentId) + 1).toString().padStart(5, '0')
+  return increementalId
+}
