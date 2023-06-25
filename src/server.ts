95dea474dfdec475a5d918111e390518702cd2e3
Alamgir Hossen <96404769+developeralamgirbd@users.noreply.github.com>
2023-06-25 22:54:26 +0600

    [Done] seperate logger success and error

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
-import logger from './shared/logger'
+import { infoLogger, errorLogger } from './shared/logger'
 
 const port = (config.port as string) || 8000
 const boostrap = async () => {
   try {
     await mongoose.connect(config.database_url as string)
-    logger.info(`DB is connected successfully`)
+    infoLogger.info(`DB is connected successfully`)
     app.listen(port, () => {
-      logger.info(`app listening on port ${port}`)
+      infoLogger.info(`app listening on port ${port}`)
     })
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (err: any) {
-    logger.error(err.message)
+    errorLogger.error(err.message)
   }
 }
 
