d5715e2966a4cba1f712590286f923ff48c81ba8
Alamgir Hossen <96404769+developeralamgirbd@users.noreply.github.com>
2023-06-25 22:54:26 +0600

    [Done] seperate logger success and error

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,17 +1,26 @@
-// import winston from 'winston'
+import winston from 'winston'
+import path from 'path'
+const infoLogger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
+      level: 'info',
+    }),
+  ],
+})
+const errorLogger = winston.createLogger({
+  level: 'error',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
+      level: 'error',
+    }),
+  ],
+})
 
-// const logger = winston.createLogger({
-//   level: 'error',
-//   format: winston.format.json(),
-//   transports: [
-//     //
-//     // - Write all logs with importance level of `error` or less to `error.log`
-//     // - Write all logs with importance level of `info` or less to `combined.log`
-//     //
-//     new winston.transports.Console(),
-//     new winston.transports.File({ filename: 'error.log', level: 'error' }),
-//     new winston.transports.File({ filename: 'combined.log' }),
-//   ],
-// })
-
-// export default logger
+export { infoLogger, errorLogger }
