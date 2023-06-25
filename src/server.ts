2d79ecc514bbebf06bd13bb1532b3627aa4fab33
Alamgir Hossen <96404769+developeralamgirbd@users.noreply.github.com>
2023-06-25 22:09:10 +0600

    [Done] basic logger setup

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,19 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import logger from './shared/logger'
 
 const port = (config.port as string) || 8000
 const boostrap = async () => {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log(`DB is connected successfully`)
+    logger.info(`DB is connected successfully`)
     app.listen(port, () => {
-      console.log(`app listening on port ${port}`)
+      logger.info(`app listening on port ${port}`)
     })
-  } catch (e) {
-    console.log(e)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    logger.error(err.message)
   }
 }
 
--- src/test.ts
+++
@@ -1,2 +0,0 @@
-const hello = 'Hello world'
-console.log(hello)
