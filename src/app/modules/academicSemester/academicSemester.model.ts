a321fca236f1c6b46a0370f8ec6f9a3224946a89
Alamgir Hossen <96404769+developeralamgirbd@users.noreply.github.com>
2023-07-11 20:59:39 +0600

    [Done] handle same title & same year issue in academic semester

--- src/app/modules/academicSemester/academicSemester.model.ts
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -8,6 +8,8 @@ import {
   academicSemesterMonths,
   academicSemesterTitle,
 } from './academicSemester.constant';
+import ApiError from '../../../errors/ApiError';
+import { StatusCodes } from 'http-status-codes';
 
 const academicSemisterSchema = new Schema<IAcademicSemister>(
   {
@@ -38,6 +40,21 @@ const academicSemisterSchema = new Schema<IAcademicSemister>(
   },
   { timestamps: true, versionKey: false }
 );
+
+academicSemisterSchema.pre('save', async function (next) {
+  const isExist = await AcademicSemister.findOne({
+    title: this.title,
+    year: this.year,
+  });
+
+  if (isExist) {
+    throw new ApiError(
+      StatusCodes.CONFLICT,
+      'Academic semester is already exist !'
+    );
+  }
+  next();
+});
 
 const AcademicSemister = model<IAcademicSemister, AcademicSemisterModel>(
   'AcademicSemister',
