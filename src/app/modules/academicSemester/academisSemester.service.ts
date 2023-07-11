34cfd81cf54c0d8a6282554f34880d3ff55ed15b
Alamgir Hossen <96404769+developeralamgirbd@users.noreply.github.com>
2023-07-11 21:25:10 +0600

    [Done] academic semester code validation

--- src/app/modules/academicSemester/academisSemester.service.ts
+++ src/app/modules/academicSemester/academisSemester.service.ts
@@ -1,9 +1,16 @@
+import { StatusCodes } from 'http-status-codes';
+import ApiError from '../../../errors/ApiError';
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
 import { IAcademicSemister } from './academicSemester.interface';
 import AcademicSemister from './academicSemester.model';
 
 const createSemester = async (
   payload: IAcademicSemister
 ): Promise<IAcademicSemister> => {
+  if (academicSemesterTitleCodeMapper[payload.title] !== payload.code) {
+    throw new ApiError(StatusCodes.BAD_REQUEST, 'Invalid semester code!');
+  }
+
   const result = await AcademicSemister.create(payload);
   return result;
 };
