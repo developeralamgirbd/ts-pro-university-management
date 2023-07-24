94bced2fc52a0ca212097c17f049669e3228b935
Alamgir Hossen <96404769+developeralamgirbd@users.noreply.github.com>
2023-07-24 07:42:40 +0600

    [Done] handle not found route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 const app: Express = express();
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import routes from './app/routes';
+import { StatusCodes } from 'http-status-codes';
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,4 +16,19 @@ app.get('/', async (req: Request, res: Response) => {
 });
 
 app.use(globalErrorHandler);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'Api Not Found',
+      },
+    ],
+  });
+  next();
+});
+
 export default app;
