eb7b9bdf3ccbcb6e544d472a79f16fe22b22d04e
Alamgir Hossen <96404769+developeralamgirbd@users.noreply.github.com>
2023-08-14 16:50:39 +0600

    [Done] student, faculty, admin genereated method

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-console */
--- src/errors/handleDuplicateError.ts
+++ src/errors/handleDuplicateError.ts
@@ -3,20 +3,20 @@ import { IGenericErrorMessage } from '../interfaces/error';
 
 const handleDuplicateError = (err: mongoose.mongo.MongoServerError) => {
   const errors: IGenericErrorMessage[] = [];
-  if (err.codeName === 'DuplicateKey') {
+  if (err.code === 11000) {
     errors.push({
       path: Object.keys(err.keyPattern)[0],
-      message: `updated ${Object.keys(err.keyPattern)[0]} already exists!`,
+      message: `Your provided ${
+        Object.keys(err.keyPattern)[0]
+      } already exists!`,
     });
   }
 
-  const statusCode = err.codeName === 'DuplicateKey' ? 400 : 500;
+  const statusCode = err.code === 11000 ? 400 : 500;
   return {
     statusCode,
     message:
-      err.codeName === 'DuplicateKey'
-        ? 'Duplicate Key Error'
-        : 'Something went wrong!',
+      err.code === 11000 ? 'Duplicate Key Error' : 'Something went wrong!',
     errorMessages: errors,
   };
 };
