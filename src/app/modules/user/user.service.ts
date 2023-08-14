2b151e89beefb43e267cfbe39c526ff39306b237
Alamgir Hossen <96404769+developeralamgirbd@users.noreply.github.com>
2023-08-14 16:50:39 +0600

    [Done] student, faculty, admin genereated method

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,33 +1,58 @@
-import config from '../../../config'
-import ApiError from '../../../errors/ApiError'
-import { generateUserId } from './user.utils'
-import { IUser } from './user.interface'
-import User from './user.model'
+import config from '../../../config';
+import ApiError from '../../../errors/ApiError';
+import { generateAdminId } from './user.utils';
+import { IUser } from './user.interface';
+import User from './user.model';
 
-export const findLastUser = async (): Promise<string | undefined> => {
-  const user = await User.findOne({}, { id: 1, _id: 0 })
+// export const findLastUser = async (): Promise<string | undefined> => {
+//   const user = await User.findOne({}, { id: 1, _id: 0 })
+//     .sort({ createdAt: -1 })
+//     .lean()
+//   return user?.id
+// }
+export const findLastStudentId = async (): Promise<string | undefined> => {
+  const lastStudent = await User.findOne({ role: 'student' }, { id: 1, _id: 0 })
     .sort({ createdAt: -1 })
-    .lean()
-  return user?.id
-}
+    .lean();
+  return lastStudent?.id ? lastStudent?.id.substring(4) : undefined;
+};
+export const findLastFacultyId = async (): Promise<string | undefined> => {
+  const lastFaculty = await User.findOne({ role: 'faculty' }, { id: 1, _id: 0 })
+    .sort({ createdAt: -1 })
+    .lean();
+  return lastFaculty?.id ? lastFaculty?.id?.substring(2) : undefined;
+};
+
+export const findLastAdminId = async (): Promise<string | undefined> => {
+  const lastAdmin = await User.findOne({ role: 'admin' }, { id: 1, _id: 0 })
+    .sort({ createdAt: -1 })
+    .lean();
+  return lastAdmin?.id ? lastAdmin?.id?.substring(2) : undefined;
+};
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
   //auto generated increemental id
-  const id = await generateUserId()
-  user.id = id
+  // const semester = {
+  //   year: '2023',
+  //   code: '01',
+  // };
+  // const id = await generateStudentId(semester);
+  // const id = await generateFacultyId();
+  const id = await generateAdminId();
+  user.id = id;
   //default password
   if (!user.password) {
-    user.password = config.default_user_pass as string
+    user.password = config.default_user_pass as string;
   }
 
-  const createdUser = await User.create(user)
+  const createdUser = await User.create(user);
 
   if (!createdUser) {
-    throw new ApiError(400, 'Failed to create user')
+    throw new ApiError(400, 'Failed to create user');
   }
-  return createdUser
-}
+  return createdUser;
+};
 
 export const userService = {
   createUser,
-}
+};
--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -1,8 +1,46 @@
-import { findLastUser } from './user.service'
+import { IAcademicSemister } from '../academicSemester/academicSemester.interface';
+import {
+  findLastAdminId,
+  findLastFacultyId,
+  findLastStudentId,
+} from './user.service';
 
-export const generateUserId = async (): Promise<string> => {
-  const currentId = (await findLastUser()) || (0).toString().padStart(5, '0')
+// export const generateUserId = async (): Promise<string> => {
+//   const currentId = (await findLastUser()) || (0).toString().padStart(5, '0');
+//   // increement id by 1
+//   const increementalId = (parseInt(currentId) + 1).toString().padStart(5, '0');
+//   return increementalId;
+// };
+
+export const generateStudentId = async (
+  academicSemester: IAcademicSemister
+): Promise<string> => {
+  const currentId =
+    (await findLastStudentId()) || (0).toString().padStart(5, '0');
   // increement id by 1
-  const increementalId = (parseInt(currentId) + 1).toString().padStart(5, '0')
-  return increementalId
-}
+  let increementalId = (parseInt(currentId) + 1).toString().padStart(5, '0');
+
+  increementalId = `${academicSemester.year.substring(2)}${
+    academicSemester.code
+  }${increementalId}`;
+
+  return increementalId;
+};
+
+export const generateFacultyId = async (): Promise<string> => {
+  const currentId =
+    (await findLastFacultyId()) || (0).toString().padStart(5, '0');
+  // increement id by 1
+  const increementalId =
+    'F-' + (parseInt(currentId) + 1).toString().padStart(5, '0');
+  return increementalId;
+};
+
+export const generateAdminId = async (): Promise<string> => {
+  const currentId =
+    (await findLastAdminId()) || (0).toString().padStart(5, '0');
+  // increement id by 1
+  const increementalId =
+    'A-' + (parseInt(currentId) + 1).toString().padStart(5, '0');
+  return increementalId;
+};
