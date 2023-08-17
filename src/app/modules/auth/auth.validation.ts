7a8d55ee7c9b2ce84851e9d23bc60e660a6ed3b8
Alamgir Hossen <96404769+developeralamgirbd@users.noreply.github.com>
2023-08-17 15:01:24 +0600

    [Done] make refresh token handler

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -1,12 +1,19 @@
 import { z } from 'zod';
 
 const loginZodValidation = z.object({
-    body: z.object({
-        id: z.string({required_error: 'ID is required!'}),
-        password: z.string({required_error: 'Password is required!'}),
-    }),
+  body: z.object({
+    id: z.string({ required_error: 'ID is required!' }),
+    password: z.string({ required_error: 'Password is required!' }),
+  }),
+});
+
+const refreshTokenZodValidation = z.object({
+  cookies: z.object({
+    refresh_token: z.string({ required_error: 'Refresh token is required!' }),
+  }),
 });
 
 export const AuthValidation = {
-    loginZodValidation,
+  loginZodValidation,
+  refreshTokenZodValidation,
 };
